64f4a3fcce3814287c6686040931c9ca7320dcf1
VoDkAdRiNkErBoI <60256507+VoDkAdRiNkErBoI@users.noreply.github.com>
2020-01-24 10:28:05 +0000

    Add files via upload

---
+++ burn em.py
@@ -0,0 +1,25 @@
+import time
+import pygame
+import os
+
+pygame.init()
+
+screen = pygame.display.set_mode((250,150))
+pygame.display.set_caption('Message From Admin')
+font = pygame.font.SysFont("Lucida Console", 20)
+label = font.render("BURN THE TORIES", 1, (0,0,0))
+while True:
+    for event in pygame.event.get():
+        if event.type==pygame.QUIT:
+            pygame.quit()
+            time.sleep(0.10)
+
+            screen = pygame.display.set_mode((250,150))
+            pygame.display.set_ caption('Message From Admin')
+
+
+    screen.fill((255,255,255))
+    screen.blit(labbel, (50,50))
+
+    pygame.display.update()
+    
