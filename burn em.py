c388b5ff4351efc3215ae57cacc3ee75d8216f79
VoDkAdRiNkErBoI <60256507+VoDkAdRiNkErBoI@users.noreply.github.com>
2020-01-26 08:36:01 +0000

    Update burn em.py

--- burn em.py
+++ burn em.py
@@ -12,12 +12,12 @@ pygame.init()
 screen = pygame.display.set_mode((250,150))
 pygame.display.set_caption('Message From Admin')
 font = pygame.font.SysFont("Lucida Console", 20)
-label = font.render("U R A MORON", 1, (0,0,0))
+label = font.render("Ello Governor", 1, (0,0,0))
 while True:
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
             pygame.quit()
-            time.sleep(0.10)
+            time.sleep(0.5)
 
             screen = pygame.display.set_mode((250,150))
             pygame.display.set_ caption('Message From Admin')
